3d7e3463cd3a11c212f25b9cee3b3ce6d7e332be
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-09 03:53:41 +0300

    Task5dz

---
+++ Task5dz/Program.cs
@@ -0,0 +1,50 @@
+﻿
+// Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+using System.Drawing;
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] arr = CreateArrayRndInt(10, 100, 1000);
+PrintArray(arr);
+
+int evenCount = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
